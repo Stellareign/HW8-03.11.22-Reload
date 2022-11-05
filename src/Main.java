aef3a8cedf1ceefb4f8b22fa128b8e4fb5a34436
Stellareign <tuguran@mail.ru>
2022-11-05 18:22:19 +0300

    Часть 2, Задача 3

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Домашка 8, часть 2, задача 2, циклы, накопления каждые полгода:");
+        System.out.println("Домашка 8, часть 2, задача 2, циклы, накопления, вывод каждые полгода:");
         int deposit = 15000;
         double saving = 0;
         double percentMonth = 0.07;
         saving = deposit;
-        int month = 1;
-        for (; saving <= 12000_000; month++) {
+        int month = 0;
+        int periodYears = 9;
+        for (month = 1; month <= 12 * periodYears; month++) {
             saving = saving + deposit * percentMonth;
-            if (i % 6 == 0) {
-
+            if (month % 6 == 0) {
                 if (saving < 1000_000) {
                     System.out.print("Месяц " + month + " - сумма накоплений равна " + saving + " руб. ");
                 } else if (saving > 1000_000) {
                     System.out.print("Месяц " + month + " - сумма накоплений равна " + saving / 1000_000 + " млн. руб. ");
+                    // Вывод в строку для большей компактности.
+                    // Как сделать, чтобы выводилось меньше знаков после запятой?
                 }
             }
         }
