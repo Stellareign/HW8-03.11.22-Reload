209b2ca1071efb997fceedbc999b97cf68da5ac2
Stellareign <tuguran@mail.ru>
2022-11-05 18:13:55 +0300

    Часть 1, Задачи 1-3

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Домашка 8, часть 1, задача 1, WHILE накопления с процентами:");
-        int deposit = 15_000;
-        double saving = 0;
-        double percentMonth = 1.01;
-        int i = 1;
-        while (saving <= 2_459_000) {
-            saving = deposit + saving*percentMonth;
-            System.out.print("Месяц " + (i++) + " - сумма накоплений равна " + saving + ". ");
-        }
-        System.out.println("" +
-                "");
-        System.out.println("Домашка 8, часть 1, задача 2, WHILE/FOR числа от 1 до 10:"); // утеряна в гите
-        int number = 1;
-        while (number <= 10) {
-            System.out.print((number++) + ("; "));
-        }
-        System.out.println("" +
-                "");
-        for (number = 10; number >= 1; number--) {
-            System.out.print(number + "; ");
-        }
 
-        System.out.println("" +
-                "");
-        System.out.println("Домашка 8, часть 1, задача 3, WHILE/FOR - численность населения:");
-        int year = 2020;
-        int populationStart = 12_000_000;
-        int populationGrowth = 17 * (12_000_000 / 1000);
-        int populationDecline = 8 * (12_000_000 / 1000);
-        int population = populationStart + populationGrowth - populationDecline;
-        for (int period = 1; period <= 10; period++){
-            population = population + populationGrowth - populationDecline;
-            System.out.println("В году " + (year++) + " численность населения соствляет " + population +" человек.");
+        System.out.println("Домашка 8, часть 2, задача 1, WHILE накопления с процентами, без капитализации:");
+        int deposit = 15_000;
+        double percentMonth = 0.07;
+        double saving = deposit;
+        int month = 1; // Жизни Василию не хватит копить по 7 % от 15 тыс.
+        while (saving <= 120_00000) {
+            saving = saving + deposit * percentMonth;
+            if (saving < 1000_000) {
+                System.out.print("Месяц " + (month++) + " - сумма накоплений равна " + saving + " руб. "); // вывод в строку для компакности
+            }
+            if (saving > 1000_000) {
+                System.out.print("Месяц " + (month++) + " - сумма накоплений равна " + saving / 1000_000 + " млн. руб. ");
+            }
         }
     }
 }
