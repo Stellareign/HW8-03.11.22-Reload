2cb02e4b439a3d6c1110412fe98f22bfd5be398c
Stellareign <tuguran@mail.ru>
2022-11-05 18:26:46 +0300

    Часть 3, Задача 1, 2

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Домашка 8, часть 2, задача 4, циклы:" +
-                "Написать программу, которая считает дни месяца по датам, определяет, " +
-                "какой день пятница, и выводит сообщение с напоминанием, что нужно " +
-                "подготовить еженедельный отчет.");
-        int firstFriday = 3;
-        for (int monthDay = 1; monthDay <= 31; monthDay++) {
-            for (int everyFriday = firstFriday; everyFriday <= 31; everyFriday = everyFriday + 7) {
-                if (monthDay == everyFriday) {
-                    System.out.println("Сегодня пятница, " + monthDay + " число. Необходимо подготовить отчет.");
-                }
+        System.out.println("Домашка 8, часть 3, задача 1, циклы, про кометы:");
+        int presentYear = 2022;
+        int futureYear = presentYear + 100; // сто лет вперёд
+        int pastYear = 200; // 200 лет назад
+        for  (int year = 0; year <= futureYear; year = year + 79) {
+            if (year >= presentYear - pastYear) {
+                System.out.println(year);
             }
+        }
+        System.out.println("Домашка 8, часть 3, задача 2, циклы, таблица умножени, х2:");
+        int multiplier = 2;
+        int number = 1;
+        int increase;
+        for (number = 1; number <= 10; number++) {
+            increase = number * multiplier;
+            System.out.println(multiplier + " * " + number + " = " + increase);
         }
     }
 }
