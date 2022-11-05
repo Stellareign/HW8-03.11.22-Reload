8c65d36fe0712ce8bbb79015beb81ad0aa331276
Stellareign <tuguran@mail.ru>
2022-11-05 18:25:01 +0300

    Часть 2, Задача 4

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Домашка 8, часть 2, задача 2, циклы, накопления каждые полгода:");
-        int deposit = 15000;
-        double saving = 0;
-        double percentMonth = 0.07;
-        saving = deposit;
-        int month = 1;
-        for (; saving <= 12000_000; month++) {
-            saving = saving + deposit * percentMonth;
-            if (month % 6 == 0) {
-
-                if (saving < 1000_000) {
-                    System.out.print("Месяц " + month + " - сумма накоплений равна " + saving + " руб. ");
-                } else if (saving > 1000_000) {
-                    System.out.print("Месяц " + month + " - сумма накоплений равна " + saving / 1000_000 + " млн. руб. ");
+        System.out.println("Домашка 8, часть 2, задача 4, циклы:" +
+                "Написать программу, которая считает дни месяца по датам, определяет, " +
+                "какой день пятница, и выводит сообщение с напоминанием, что нужно " +
+                "подготовить еженедельный отчет.");
+        int firstFriday = 3;
+        for (int monthDay = 1; monthDay <= 31; monthDay++) {
+            for (int everyFriday = firstFriday; everyFriday <= 31; everyFriday = everyFriday + 7) {
+                if (monthDay == everyFriday) {
+                    System.out.println("Сегодня пятница, " + monthDay + " число. Необходимо подготовить отчет.");
                 }
             }
         }
